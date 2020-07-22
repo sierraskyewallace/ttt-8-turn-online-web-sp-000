14b0faf1ad6ab58030333100d04f4f557156969e
Sierra Wallace <sskwallace@gmail.com>
2020-07-22 01:15:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, player(board))
-    display_board(board)
+  else
+    turn(board)
   end
 end
 
