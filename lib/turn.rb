58addb8cbf336635f94bfab63d3b7c5eefe51998
Sierra Wallace <sskwallace@gmail.com>
2020-07-22 00:54:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,18 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def display_board(board)
- 
- puts " #{board[0]} | #{board[1]} | #{board[2]} "
- puts "-----------"
- puts " #{board[3]} | #{board[4]} | #{board[5]} "
- puts "-----------"
- puts " #{board[6]} | #{board[7]} | #{board[8]} "
- 
- board[4] = "X"
- end
- 
- 
- 
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, player = "X")
+  board[index] = player
+end
+
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
