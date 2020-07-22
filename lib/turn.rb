d48a3cb901261151a04c1639d59c11a7fe24d73e
Sierra Wallace <sskwallace@gmail.com>
2020-07-22 00:55:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,13 @@ end
 
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board, index)
+  if board[index] == " " ||  board[index] == "" || board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+
